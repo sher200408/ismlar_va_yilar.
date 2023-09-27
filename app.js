d4fa9beace48df9c3a9fc4c1420e667b111dcdc0
shermuhammad shertayev backend <113122559+sher200408@users.noreply.github.com>
2023-09-27 10:10:27 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,10 @@
+function boxThe() {
+    let ism = document.querySelector("#g2");
+    let plar = document.querySelector("#g5");
+    plar.innerHTML = (ism.value+"bu siz ismgiz")
+
+    let yilgiz = document.querySelector("#g3");
+    let plan = document.querySelector("#g6");
+    let dox = 2023-yilgiz.value
+    plan.innerHTML = (dox +" bu siz yoshgiz")
+}
